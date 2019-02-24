40eb2a1540dcd9c423f24d8b5037e50e7e96862c
Hussein Azeez <hussein.azeez91@gmail.com>
2019-02-24 23:26:59 +0700

    Intial commit

---
+++ server/config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+  APPLICATION_ID: process.env.APPLICATION_ID || 'b104d112a2ba0d59b5fbaeff39b0ddf9392a3f81c9856f24507311f9eb425b31',
+  SECRET: process.env.SECRET || '1dd01bedfd24dbca36ec451e76db6318706ba1d6895304eb0bfd6fa334d5c9fc',
+  CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+}
